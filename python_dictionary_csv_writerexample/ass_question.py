c1a6b0b64ba54b4d3ff95b510688b73623136c67
seanwayland <seanwayland@gmail.com>
2021-10-24 13:14:58 -0400

    added assign question

--- python_dictionary_csv_writerexample/ass_question.py
+++ python_dictionary_csv_writerexample/ass_question.py
@@ -29,3 +29,16 @@ with open('sample_data.csv', newline='') as csvfile:
 print("results")
 print(results_dict)
 
+results = results_dict 
+totalcountyvotes = sum(results['counties_dict'].values())
+print("total county votes")
+print(totalcountyvotes)
+
+for k, v in results['counties_dict'].items():
+    print("votes per county")
+    print(k, v)
+    print("percent voters per county")
+    print(k, v/totalcountyvotes * 100)
+
+print("winning county is ")
+print(max(results['counties_dict'], key=results['counties_dict'].get))
